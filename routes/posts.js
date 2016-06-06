753ce1593e4f928d7580122e1b362fcb0060e75c
David <goodwid@gmail.com>
2016-06-06 16:36:59 -0700

    working on posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,18 +1,32 @@
 const router = require('express').Router();
 const bodyParser = require('body-parser').json();
 const Post = require('../models/post');
-// const token = require('../lib/token');
 const isAuth = require('../lib/ensureAuth');
 
 router
   .get('/list', bodyParser, isAuth, (req, res, next) => {
-    console.log(req.user);
     const query = req.query.type;
     Post.find(query)
-      .select('body author topics event link image')
-      .populate('author', '{username: , email}')
+      // .select('body author topics event link image')
+      .populate('author')
+      .populate('topics')
+      .populate('event')
       .lean()
-      .then(results => res.json(results))
+      .then(results => {
+        results.forEach(r => {
+          delete r.author.password;
+          if (req.user.roles.indexOf('admin') === -1) {
+            if (r.author.email_hidden) delete r.author.email;
+            if (r.author.profile_twitter_hidden) delete r.author.twitter_username;
+          }
+          delete r.author.email_hidden;
+          delete r.author.profile_twitter_hidden;
+          delete r.author.agenda;
+          delete r.author.roles;
+        });
+        res.json(results);
+      })
+
       .catch(err => next(err));
   })
   .post('/', bodyParser, (req, res, next) => {
