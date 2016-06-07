669031b520683faf948394ea374007c7824c2876
Allison Davis <a.rae.davis@gmail.com>
2016-06-07 15:21:28 -0700

    posts template render

--- public/js/views/postView.js
+++ public/js/views/postView.js
@@ -1,10 +1,23 @@
 (function (module){
 
+  function renderTemplate(post){
+    var template = Handlebars.compile($('#post-template').text());
+    var htmlObject = template(post);
+
+    return htmlObject;
+  }
+
+
   function renderPage(){
     retrieveAllPosts(function(arrayOfPosts){
       console.log(arrayOfPosts);
+      arrayOfPosts.forEach(function(post){
+
+        $('#all-posts').append(renderTemplate(post));
+      });
     });
   }
 
   module.renderPage = renderPage;
+  module.renderTemplate = renderTemplate;
 })(window);
