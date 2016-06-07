23de28c7910659743dddba73be75c847a2aa1c25
David <goodwid@gmail.com>
2016-06-07 10:35:24 -0700

    one more edit to posts route

--- routes/posts.js
+++ routes/posts.js
@@ -6,24 +6,26 @@ router
   .get('/list', bodyParser, (req, res, next) => {
     const query = req.query.type;
     Post.find(query)
-      // .select('body author topics event link image')
       .populate('author')
       .populate('topics')
       .populate('event')
       .lean()
       .then(results => {
-        results.forEach(r => {
-          delete r.author.password;
-          if (req.user.roles.indexOf('admin') === -1) {
-            if (r.author.email_hidden) delete r.author.email;
-            if (r.author.profile_twitter_hidden) delete r.author.profile_twitter_username;
-          }
-          delete r.author.email_hidden;
-          delete r.author.profile_twitter_hidden;
-          delete r.author.agenda;
-          delete r.author.roles;
-        });
-        res.json(results);
+        console.log(results);
+        if (results.length < 0) {
+          results.forEach(r => {
+            delete r.author.password;
+            if (req.user.roles.indexOf('admin') === -1) {
+              if (r.author.email_hidden) delete r.author.email;
+              if (r.author.profile_twitter_hidden) delete r.author.profile_twitter_username;
+            }
+            delete r.author.email_hidden;
+            delete r.author.profile_twitter_hidden;
+            delete r.author.agenda;
+            delete r.author.roles;
+          });
+          res.json(results);
+        } else next({code: 404,error:'no posts found'});
       })
 
       .catch(err => next(err));
