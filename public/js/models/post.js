5e3af7ecd5f69ace473052cc0b6e75182df4686a
Allison Davis <a.rae.davis@gmail.com>
2016-06-07 16:08:56 -0700

    add new posts form

--- public/js/models/post.js
+++ public/js/models/post.js
@@ -6,12 +6,6 @@
     }, this);
   }
 
-  Post.prototype.toHtml = function(){
-    
-
-
-  };
-
   // const tokenForTesting = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU3NTVmOTIyNGY4ZjgxNmZhYzRhYTY3OSIsInJvbGVzIjpbImFkbWluIl0sImlhdCI6MTQ2NTI1MjU0OH0.EgUa-3c1YJdYJetW5q7WJC9galDbxxCVsqhjdSbI1iA';
 
   function retrieveAllPosts(callback){
--- public/js/views/postView.js
+++ public/js/views/postView.js
@@ -7,8 +7,11 @@
     return htmlObject;
   }
 
-
   function renderPage(){
+    // $('#new-post').hide();
+    // $('#events').hide();
+    // $('#my-profile').hide();
+
     retrieveAllPosts(function(arrayOfPosts){
       console.log(arrayOfPosts);
       arrayOfPosts.forEach(function(post){
@@ -17,6 +20,12 @@
       });
     });
   }
+
+
+
+  //TODO add filter by topics
+  //TODO
+  //
 
   module.renderPage = renderPage;
   module.renderTemplate = renderTemplate;
