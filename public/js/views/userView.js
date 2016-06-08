7ed9e6112f100ca50373be9d51f4a8f3952ec672
Allison Davis <a.rae.davis@gmail.com>
2016-06-08 16:30:58 -0700

    user ui bug fixes

--- public/js/views/userView.js
+++ public/js/views/userView.js
@@ -11,15 +11,23 @@
         //retrieve user data
         var url = `/users/${userId}`;
         API.getOne(url, User, function(user){
-
-          if(user.organization === undefined){
+          console.log(user);
+          if(!user.organization){
             user.organization ='Organization Unknown';
           }
+
+          if(!user.profile){
+            $('.user-img-container').html('<img src="./img/user-img-default.jpg">');
+          }
+
           //populate user data onto modal window
           $('.modal-title').text(`${user.firstName} ${user.lastName}`);
           $('.user-org').text(user.organization);
 
-
+          //TODO create better handling for hidden/unspecified contact info
+          $('.user-website').attr('href', user.profile.website);
+          $('.user-email').attr('href', user.profile.email);
+          $('.user-twitter').attr('href', user.profile.twitter);
 
         });
       });
