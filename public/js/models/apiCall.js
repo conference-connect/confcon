49bef2cfc83f4e4ae819ff360688131a6f704478
David <goodwid@gmail.com>
2016-06-08 10:40:46 -0700

    API calls in each model genericized to one API call library

---
+++ public/js/models/apiCall.js
@@ -0,0 +1,72 @@
+(function(module){
+
+  var API = {
+    retrieveAll (url, Item, callback) {
+      $.ajax({
+        url: url,
+        type: 'GET',
+        headers: {token: localStorage.token}})
+      .done(function(data){
+        var newArray = data.map(function(el){
+          return new Item(el);
+        });
+        callback(newArray);
+      })
+      .fail(function(){
+        console.log('RetrieveAll: failure to complete ajax call to ' + url);
+        callback([]);
+      });
+    },
+    makeNew (url, data, Item, callback){
+      $.ajax({
+        url: url,
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        headers: {token: localStorage.token}})
+      .done(function(data){
+        var returnObject = new Item(data);
+        callback(returnObject);
+      })
+      .fail(function(){
+        console.log('makeNew: failure to complete ajax call to ' + url);
+        callback({});
+      });
+    },
+    edit (url, data, Item, callback) {
+      $.ajax({
+        url: url,
+        type:'PATCH',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        headers: {token: localStorage.token}})
+      .done(function(data){
+        var returnObject = new Item(data);
+        callback(returnObject);
+      })
+      .fail(function(){
+        console.log('Edit: failure to complete ajax call to ' + url);
+        callback({});
+      });
+    },
+    delete (url, Item, callback) {
+      $.ajax({
+        url: url,
+        type:'DELETE',
+        headers: {token: localStorage.token}})
+      .done(function(data){
+        var returnObject = new Item(data);
+        callback(returnObject);
+      })
+      .fail(function(){
+        console.log('Delete: failure to complete ajax call to ' + url);
+        callback({});
+      });
+    }
+
+
+  };
+
+  module.API = API;
+
+})(window);
--- public/js/models/post.js
+++ public/js/models/post.js
@@ -6,82 +6,77 @@
     }, this);
   }
 
-
-  // const tokenForTesting = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU3NTVmOTIyNGY4ZjgxNmZhYzRhYTY3OSIsInJvbGVzIjpbImFkbWluIl0sImlhdCI6MTQ2NTI1MjU0OH0.EgUa-3c1YJdYJetW5q7WJC9galDbxxCVsqhjdSbI1iA';
   Post.prototype.toHtml = function(){
-
-
-
   };
 
-  Post.retrieveAll = function (callback){
-    const tokenFromStorage = localStorage.token;
-    $.ajax({url:'/api/post/list', headers: {'token': tokenFromStorage}}, {method:'GET'})
-    .done(function(data){
-      var newArray = data.map(function(el){
-        return new Post(el);
-      });
-      callback(newArray);
-    })
-    .fail(function(){
-      console.log('failure to complete ajax call in retrieveAllPosts');
-      callback([]);
-    });
-  };
-
-  Post.makeNew = function(postData, callback){
-    const tokenFromStorage = localStorage.token;
-    $.ajax({
-      url:'/api/post/',
-      type:'POST',
-      contentType: 'application/json',
-      data: JSON.stringify(postData),
-      headers: {'token': tokenFromStorage}})
-    .done(function(data){
-      var returnObject = new Post(data);
-      callback(returnObject);
-    })
-    .fail(function(){
-      console.log('failure to complete ajax in makeNewPost');
-      callback({});
-    });
-  };
-
-  Post.edit = function(postID, postData, callback){
-    const tokenFromStorage = localStorage.token;
-    const updateUrl = `/api/post/${postID}`;
-    $.ajax({
-      url:updateUrl,
-      type:'PATCH',
-      contentType: 'application/json',
-      data: JSON.stringify(postData),
-      headers: {'token': tokenFromStorage}})
-    .done(function(data){
-      var returnObject = new Post(data);
-      callback(returnObject);
-    })
-    .fail(function(){
-      console.log('failure to complete ajax in editPost');
-      callback({});
-    });
-  };
-
-  Post.delete = function(postID, callback){
-    const tokenFromStorage = localStorage.token;
-    const deleteUrl = `/api/post/${postID}`;
-    $.ajax({
-      url:deleteUrl,
-      type:'DELETE',
-      headers: {'token': tokenFromStorage}})
-    .done(function(data){
-      var returnObject = new Post(data);
-      callback(returnObject);
-    })
-    .fail(function(){
-      console.log('failure to complete ajax in deletePost');
-      callback({});
-    });
-  };
+  // Post.retrieveAll = function (callback){
+  //   var tokenFromStorage = localStorage.token;
+  //   $.ajax({url:'/api/post/list', headers: {'token': tokenFromStorage}}, {method:'GET'})
+  //   .done(function(data){
+  //     var newArray = data.map(function(el){
+  //       return new Post(el);
+  //     });
+  //     callback(newArray);
+  //   })
+  //   .fail(function(){
+  //     console.log('failure to complete ajax call in retrieveAllPosts');
+  //     callback([]);
+  //   });
+  // };
+  //
+  // Post.makeNew = function(postData, callback){
+  //   var tokenFromStorage = localStorage.token;
+  //   $.ajax({
+  //     url:'/api/post/',
+  //     type:'POST',
+  //     contentType: 'application/json',
+  //     data: JSON.stringify(postData),
+  //     headers: {'token': tokenFromStorage}})
+  //   .done(function(data){
+  //     var returnObject = new Post(data);
+  //     callback(returnObject);
+  //   })
+  //   .fail(function(){
+  //     console.log('failure to complete ajax in makeNewPost');
+  //     callback({});
+  //   });
+  // };
+  //
+  // Post.edit = function(postID, postData, callback){
+  //   var tokenFromStorage = localStorage.token;
+  //   var updateUrl = `/api/post/${postID}`;
+  //   $.ajax({
+  //     url:updateUrl,
+  //     type:'PATCH',
+  //     contentType: 'application/json',
+  //     data: JSON.stringify(postData),
+  //     headers: {'token': tokenFromStorage}})
+  //   .done(function(data){
+  //     var returnObject = new Post(data);
+  //     callback(returnObject);
+  //   })
+  //   .fail(function(){
+  //     console.log('failure to complete ajax in editPost');
+  //     callback({});
+  //   });
+  // };
+  //
+  // Post.delete = function(postID, callback){
+  //   var tokenFromStorage = localStorage.token;
+  //   var deleteUrl = `/api/post/${postID}`;
+  //   $.ajax({
+  //     url:deleteUrl,
+  //     type:'DELETE',
+  //     headers: {'token': tokenFromStorage}})
+  //   .done(function(data){
+  //     var returnObject = new Post(data);
+  //     callback(returnObject);
+  //   })
+  //   .fail(function(){
+  //     console.log('failure to complete ajax in deletePost');
+  //     callback({});
+  //   });
+  // };
 
   module.Post = Post;
 
