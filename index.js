72fc7d4e61783c9ca49fe5d2d2ab4bd012e53990
David <goodwid@gmail.com>
2016-06-10 12:10:44 -0700

    Added instructions to readme

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 require ('./lib/setup-mongoose');
 const app = require('./lib/app');
-const port = process.env.PORT || process.argv[2];
+const port = process.env.PORT || process.argv[2] || 9000;
 
 app.listen(port, () => {
   console.log(`server listening on port ${port}`);
